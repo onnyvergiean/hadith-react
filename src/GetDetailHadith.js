b6bdd255bfca9bbab4c81492b4ccb2d11a2ee4c1
Onny Vergiean Saputra <onny.s@students.amikom.ac.id>
2022-02-11 21:15:19 +0700

    Add Spinner when loading data

--- src/GetDetailHadith.js
+++ src/GetDetailHadith.js
@@ -7,12 +7,14 @@ import {
 import HadithDetail from "./HadithDetail";
 import HeadingDetail from "./HeadingDetail";
 import Navigator from "./Navigator";
+import Spinner from "./Spinner";
 import "./HadithList.css";
 import axios from "axios";
 
 export default function GetHadithDetail(props) {
   const [detailHadith, setDetailHadith] = useState([]);
   const [hadithName, setHadithName] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   let { id } = useParams();
   const [hadithNumber, setHadithNumber] = useState(1);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -25,6 +27,7 @@ export default function GetHadithDetail(props) {
         setSearchParams(createSearchParams({ no: hadithNumber }));
         setDetailHadith(res.data.data.contents);
         setHadithName(res.data.data.name);
+        setIsLoading(false);
       };
 
       fetchData();
@@ -37,15 +40,21 @@ export default function GetHadithDetail(props) {
 
   const search = (newNumber) => {
     setHadithNumber(newNumber);
+    setIsLoading(false);
   };
 
   const nextHadith = () => {
     setHadithNumber(hadithNumber + 1);
+    setIsLoading(true);
   };
   const backHadith = () => {
     setHadithNumber(hadithNumber - 1);
+    setIsLoading(true);
   };
-  if (detailHadith) {
+
+  if (isLoading) {
+    return <Spinner />;
+  } else if (detailHadith) {
     return (
       <>
         <div className="container p-3">
--- src/GetListHadith.js
+++ src/GetListHadith.js
@@ -2,14 +2,18 @@ import axios from "axios";
 import React, { Component } from "react";
 import HadithList from "./HadithList";
 import Heading from "./Heading";
+import Spinner from "./Spinner";
 class Hadith extends Component {
   constructor(props) {
     super(props);
-    this.state = { listhadith: [] };
+    this.state = { listhadith: [], isLoading: true };
   }
   async componentDidMount() {
     let res = await axios.get("https://api.hadith.sutanlab.id/books");
-    this.setState({ listhadith: res.data.data });
+    this.setState({
+      listhadith: res.data.data,
+      isLoading: false,
+    });
   }
   render() {
     let listhadith = this.state.listhadith.map((i) => {
@@ -22,12 +26,13 @@ class Hadith extends Component {
         />
       );
     });
+    let renderData = this.state.isLoading ? <Spinner /> : listhadith;
     return (
       <>
         <Heading />
         <div className="container mt-4">
           <div className="row text-center justify-content-center">
-            {listhadith}
+            {renderData}
           </div>
         </div>
       </>
